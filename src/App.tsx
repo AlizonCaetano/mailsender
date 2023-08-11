f6b8217d9850eaba99ee45b806917574048b825e
AlizonCaetano <alison.natan17@hotmail.com>
2023-08-11 19:34:43 -0300

    feat: add input chakra component and add css

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,21 @@
+import { useState } from "react";
 import "./App.css";
+import PrimaryInput from "./components/Input/PrimaryInput";
 
 function App() {
-  return <></>;
+  const [email, setEmail] = useState('')
+
+  return (
+    <div className='container'>
+      <PrimaryInput 
+        value={email} 
+        label="Digite seu e-mail"
+        onChange={event => setEmail(event.target.value)} 
+        name="Email"
+        />
+
+    </div>
+  )
 }
 
 export default App;
